d9526bf5df10f3a7825536956043b687470bdc0a
Ladar <1277065394@qq.com>
2024-10-21 13:45:22 +0800

    修复第二天快速点击产生的旋转混乱bug

--- My project/Assets/Script/Day2/CircleRotation.cs
+++ My project/Assets/Script/Day2/CircleRotation.cs
@@ -8,13 +8,15 @@ public class CircleRotation : MonoBehaviour
     [SerializeField]private float rotationDuration;
     public LayerMask targetLayer;
     private BoxCollider2D boxCollider;
-
+    public static bool canOperate=true;
     void Start()
     {       
         boxCollider = GetComponent<BoxCollider2D>();
     }
     void OnMouseDown()
-    {       
+    {
+        if (!canOperate) return;
+        OperateCoolDown();
         Collider2D[] collidersWithin = Physics2D.OverlapBoxAll(boxCollider.bounds.center, boxCollider.bounds.size, 0f, targetLayer);
        
         foreach (Collider2D collider in collidersWithin)
@@ -23,5 +25,10 @@ public class CircleRotation : MonoBehaviour
         }
         transform.DORotate(new Vector3(0f, 0f, transform.rotation.eulerAngles.z - 120f), rotationDuration, RotateMode.Fast);
         TimeManager.StartTimer(()=> GameEventManager.Instance.Triggered("PuzzlePass", transform, Vector3.zero),0.25f);       
+    }
+    void OperateCoolDown()
+    {
+        canOperate = false;
+        TimeManager.StartTimer(()=>canOperate=true,rotationDuration);
     }
 }       
