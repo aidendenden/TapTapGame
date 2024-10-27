f54f759a5d8da662b8791e8c994772f4b28eba97
aidendenden233 <2996876880@qq.com>
2024-10-27 21:47:56 +0800

    update

---
+++ My project/Assets/Script/UI/UIFade.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIFade : MonoBehaviour
+{
+    public Image uiImage;
+    public float fadeDuration = 1f; // 控制透明度变化的时间
+
+    void Start()
+    {
+        uiImage = GetComponent<Image>();
+        StartCoroutine(FadeInOut());
+    }
+
+    IEnumerator FadeInOut()
+    {
+        // 从0到1
+        yield return StartCoroutine(FadeImage(0f, 1f));
+        // 从1到0
+        yield return StartCoroutine(FadeImage(1f, 0f));
+    }
+
+    IEnumerator FadeImage(float startAlpha, float endAlpha)
+    {
+        float elapsedTime = 0f;
+        Color color = uiImage.color;
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Lerp(startAlpha, endAlpha, elapsedTime / fadeDuration);
+            uiImage.color = new Color(color.r, color.g, color.b, alpha);
+            yield return null;
+        }
+    }
+}
